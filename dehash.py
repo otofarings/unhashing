e36e33d1d43ff13df2ae73d3bc9ba146c2ca7c28
otofarings <75478057+otofarings@users.noreply.github.com>
2021-10-25 01:55:23 +0300

    Add files via upload

--- dehash.py
+++ dehash.py
@@ -16,62 +16,94 @@ path_small_files = '/Users/anton/Projects/Automation/Automation/Рез'
 # Указать путь для сохранения файла с результатом (включая название файла)
 path_result = '/Users/anton/Projects/Automation/Automation/Рез/final.csv'
 
-# Указать кол-во строк в большом файле
-rows_in_file = 120000000
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+class Unhash:
+
+    def __init__(self):
+
+        self.name = (path_small_files + '/ver')
+        self.format_file = path_big_file[-4:]
+
+        self.rows_in_file = sum(1 for line in open(path_big_file))
+        self.rows_in_one_file = 1000000
+        self.numbers_of_files = int(self.rows_in_file // self.rows_in_one_file + 1) \
+            if self.rows_in_file % self.rows_in_one_file != 0 \
+            else int(self.rows_in_file / self.rows_in_one_file)
+        self.rows_counter, self.count_files = 0, 0
+
+        self.small_file = None
+        self.df_for_unhash = None
+        self.lst_result = []
 
 # ---------------------------------------------------------------------------------------------------------------------
-name, format_file = (path_small_files + '/ver'), path_big_file[-4:]
-rows_in_one_file = 1000000
-numbers_of_files = int(rows_in_file // rows_in_one_file + 1) \
-    if rows_in_file % rows_in_one_file != 0 \
-    else int(rows_in_file / rows_in_one_file)
-rows_counter, count_files = 0, 0
+    def start_script(self):
+        start_time = time.time()
+        self.split_big_file()
+        self.get_file_with_hashes()
+        self.iter_throw_small_files()
+        self.save_results()
+        print(f'-------{time.time() - start_time}-------')
 
-small_file = None
-
-# ---------------------------------------------------------------------------------------------------------------------
-print('----Разбиение файла----')
-start_time = time.time()
-with open(path_big_file, "r", encoding="utf8") as in_file:
-    for hsh, number in csv.reader(in_file):
-        if rows_counter % (rows_in_file // 100) == 0:
-            print(f'Прогресс - {rows_counter / rows_in_file * 100} %')
-        if rows_counter % rows_in_one_file == 0:
+    def split_big_file(self):
+        print('----Разбиение файла----')
+        start_time = time.time()
+        with open(path_big_file, "r", encoding="utf8") as in_file:
+            for hsh, number in csv.reader(in_file):
+                if self.rows_counter % (self.rows_in_file // 100) == 0:
+                    print(f'Прогресс - {self.rows_counter / self.rows_in_file * 100} %')
+                if self.rows_counter % self.rows_in_one_file == 0:
+                    if small_file:
+                        small_file.close()
+                        print(f'файл {self.count_files} из {self.numbers_of_files} готов')
+                    self.count_files += 1
+                    small_filename = f'{self.name}_{self.count_files}{self.format_file}'
+                    small_file = open(small_filename, "w")
+                    writer = csv.writer(small_file)
+                writer.writerow((hsh, number))
+                self.rows_counter += 1
             if small_file:
                 small_file.close()
-                print(f'файл {count_files} из {numbers_of_files} готов')
-            count_files += 1
-            small_filename = f'{name}_{count_files}{format_file}'
-            small_file = open(small_filename, "w")
-            writer = csv.writer(small_file)
-        writer.writerow((hsh, number))
-        rows_counter += 1
-    if small_file:
-        small_file.close()
-print('----Готово----\n')
+        print(f'-------{time.time() - start_time}-------')
+        print('----Готово----\n')
 
-print('----Нахождение совпадений----')
-start_time = time.time()
-df1 = pd.read_csv(path_hashes, names=['hash'], index_col=False)
-df_result = []
+    def get_file_with_hashes(self):
+        start_time = time.time()
+        self.df_for_unhash = pd.read_csv(path_hashes, names=['hash'], index_col=False)
+        print(f'-------{time.time() - start_time}-------')
 
-for i, filename in enumerate(sorted(os.listdir(path_small_files))):
-    if filename.endswith('.csv'):
-        print(filename)
-        df_n = pd.read_csv(
-            f'{path_small_files}/{filename}',
-            names=['hash', 'numbers'], index_col=False
+    def iter_throw_small_files(self):
+        start_time = time.time()
+        for i, filename in enumerate(sorted(os.listdir(path_small_files))):
+            if filename.endswith('.csv'):
+                print(filename)
+                df_n = pd.read_csv(
+                    f'{path_small_files}/{filename}',
+                    names=['hash', 'numbers'], index_col=False
+                )
+                self.unhashing(df_n)
+                print(f'ok{i}')
+            if len(self.df_for_unhash) == 0:
+                break
+        print(f'-------{time.time() - start_time}-------')
+
+    def unhashing(self, df):
+        start_time = time.time()
+        self.lst_result += pd.concat([self.df_for_unhash.set_index('hash'), df.set_index('hash')],
+                                     axis=1, join='inner').reset_index()['numbers'].tolist()
+        self.df_for_unhash = pd.DataFrame(
+            {'hash': list(set(self.df_for_unhash['hash'].tolist()) - set(df['hash'].tolist()))}
         )
-        df_result += pd.concat([df1.set_index('hash'), df_n.set_index('hash')],
-                               axis=1, join='inner').reset_index()['numbers'].tolist()
-        df1 = pd.DataFrame({'hash': list(set(df1['hash'].tolist()) - set(df_n['hash'].tolist()))})
-        print(len(df_result))
-        print(len(df1))
-        print(f'ok{i}')
-    if len(df1) == 0:
-        break
-print('----Готово----\n')
+        print(f'-------{time.time() - start_time}-------')
 
 # ---------------------------------------------------------------------------------------------------------------------
-pd.DataFrame({'a': df_result}).to_csv(path_result, index=False, header=False)
-print(f'-------{time.time() - start_time}-------')
+    def save_results(self):
+        start_time = time.time()
+        pd.DataFrame({'a': self.lst_result}).to_csv(path_result, index=False, header=False)
+        print(f'-------{time.time() - start_time}-------')
+
+
+if __name__ == '__main__':
+    x = Unhash()
+    x.start_script()
